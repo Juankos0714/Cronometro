56819c5dc5a350ba6b52251074ca9ad068fbc78d
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2025-10-30 07:09:45 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { CronometroComponent } from './app/cronometro/cronometro.component';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(CronometroComponent)
   .catch((err) => console.error(err));
+
